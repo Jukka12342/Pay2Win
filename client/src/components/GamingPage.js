de1b0cc6158c223ab7d65f376df43e1362769f8b
ViktorGoodBoy <165660556+ViktorGoodBoy@users.noreply.github.com>
2024-05-16 21:07:13 +0300

    Карточка товара 1

---
+++ client/src/components/GamingPage.js
@@ -0,0 +1,56 @@
+import React from "react";
+import "../styles/GamingPage.css";
+
+const GamingPage = () => {
+
+  return (
+    <div class="container-first">
+            <header className="fitst-header">
+      <div class="container-header">
+        <div class="game-header">
+          <div class="game-img">
+          <img src="https://i.pinimg.com/564x/01/5a/17/015a1757e67f766a12621af543b3a5fd.jpg" alt="Game Image"/>
+          </div>
+            <div class="game-nav-info">
+              <h3 class="game-name">Osmanthus wine tastes the same as I remember...</h3>
+                <div class="description-game">
+                  <p class="description">Описание</p>
+                  <span class="description-text">If advertisements are to he learned, there is a need for lots of repetition. But advertisers have to be careful because too much repetition can result in consumer tiredness and the message may fall on'deal ears'.</span>
+                </div>
+            </div>
+        </div>
+      </div>
+       <div class="right-block">
+            <div class="block-side">
+                <div class="block-side-sell">
+                   <h3>Цена</h3> 
+                  <p class="block-side-text">
+                  2 000₽
+                  </p>
+                  <div className="sell-main-btn">
+                  <button class="gamebtn-hide" id="gamebtn-hide"><i class="gamebtn-hide-i"></i>Скрыть</button>
+                  <button class="gamebtn-buy" id="gamebtn-buy"><i class="gamebtn-buy-i"></i>Купить</button>
+                  </div>
+                </div>
+          </div>
+      </div> 
+      </header>
+      <div class="game-main">
+        <div className="game-main-header">
+        <div class="game-main-btn">
+              <h4>Описание</h4>
+                </div>
+            <p className="main-title-text">
+            We just opened one of my grandfather's phones. It was not the first time today. The first time in a few years (and today) was what I consider between 9:08 to 9:09 PM on Friday, December 10, 2021, but what the phone considers 21:05 on Wednesday, December 31, 1969. That was almost fifty-two years ago! Hello! I know I didn't say a "Bye!", but I think we are opening the second phone's contents. We tried to open the contents and get it from the second phone, but it said we had to wait until 1:04 pm on May 25, 2036. I wonder if 1:04 pm at May 25, 2016 was the last time my grandfather used the second phone? And then, I think, he put a lock on it for the next 20 years at that time. Anyway, we're not opening it until someone (a phone programmer) says it is okay to open that phone. Did you know that in 2016, no Longest Text Ever was being edited? Kenneth Iman's was the last at that time, until Rainbow Fluffy Sheep came in 2018 and Viba (Whiletrue) came in 2019. Actually, today is Saturday, December 11, 2021. And they fixed the time/clock on the first phone from January 1, 1970 (that's what the phone would display if it wasn't fixed) to December 11, 2021. I forgot to tell you the models, which are coming in the next sentence. The first phone's model is iPhone 3GS, and the second phone's model is the iPhone 4. And Safari still works! Even the keyboard works! Remember when I told you his birthdate was September 6, 1894? No, that was the birthdate of my great-grandfather. My real grandfather was born on July 21, 1931. And, my grandfather died on September 27, 2021. Bye! Hello there. Did you know that I have a method that can actually improve quality? It's simple. So, using my method, I have archived "You Might Think" by The Cars with better quality! Plus, it was recorded from 1983-1984, and where I got it from was the first phone, which is old, so it has bad quality. But my father (who is still an old man) says that using my method will make songs in bad quality better quality! Before that, I used the method on another song from the first phone, "Here We Are" by Gloria Estefan! Bye! Hello! I'm angry! HEHUCRFBCFEUHBFCEHUBFRCHUFRBFUHEBCFEHUFCBEUFHCBRFHJFBCUHRFVBRFHUCFRBHUCBHRFUHBCRFUHRCFBCHURFBFCDHUBCFRUHERS S CUAUAEMOEVHIIFYFSIOAUSNANABSGRGRYJDKSJSTSTEIEPEPPERJSN,SKWMF.S,G,F,EMEHSJDMANSJSKAKSSYSAJSLBE
+            CFRBRFCUHBFCRHUBFCRUHRBFCUHFBDCDUFCBCRCUHFRBCHUBFDCHUBCCRBUHDFCBFECUHFDBCHFURBDFCHUFBRCUHCFBRUHCFRBCUHFRBC WKDGONT FKFOYUROLS ENS SA S DMIDIS,DLDLIFODND RNCCHARADSCWTFRHUCBFRUHCFRBCFRHUBFRCUHRBF HURCFBFRCHUBFCHUFBRCUHFR BFRHCUBRFV IHVALLO ANGRY STEPPER IS ANGRY HE STEPPED ON ME deuhndeuhndeunHUEDNCUHEDBCUHECBHUEDBDCUHBRFUCHBFRUHCBBRFUHCB
+            RUHBCFRUHBCFEFHUVBURHBCFRUHBCFRUHBVFEUHFVEBHUEFCBUFRBCHRFUBCF
+            RUHBFCRUHFECBUHCFEBCUEFHEFBYEMABDAIDHJSOAUTNCKSISBFJSNAHS DNHSGAGRVB NSISHTIRK TTUOSONE NDUABSBN JOMMILLLION CHARSRACTERSVS KDHFNSKDIDHFJDJDFUDJSJDJDJXJSHAKSJZIj'hJjjjjjjhhj'shdkaidsjdjjihjjjk!!lm,,kk,,,,,,sjfhsmdmkedmMMmaijnagwgjNgMJnbbANgGjHjjHg nNananmMJhajhJJkGjJJjjJehdmahensanyANWGRGYEJSJGANGFRYEHAJEJ
+            KKKSDTSTENSMDJSTOSJRJSIRJWIEOEPPPERDSDXSDYOAUSHSNAJBMAMDJEKSLAKSKWONMEMAIEND AMAUEHANNAGSRSRSFAGAFARGGGFSRSTSYWHW YH NNIWASIEJREIRI AIBO WIROJTN WIRINT WTIR WIRIT WITUT WTRIN WIEIEHSKWIWWIWIWIWIWIWIWIWIWIWIWIWEUDMSKSOEIEJEJSJSSM WIRJEKW WRITUNG WIR WRINO WRITING ABOUT AIBO SIRHSKRUFJLDE,,,,lO,o!,ll,,,!,l,O,,,,,,,,,,,,!,!!!!,!,,,,,, I'm sorry for that but I was angry. But that contributed to how many characters in this Longest Text Ever.
+            </p>
+            </div>
+      </div>
+        </div>
+  );
+}
+ 
+export default GamingPage;
